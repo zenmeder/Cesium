4a45c05bd4a64086df9d8e8816426bc426c611d2
zenmeder <zenmeder@gmail.com>
2017-03-09 13:52:14 +0800

    default

--- sbadmin2/pages/getDir.js
+++ sbadmin2/pages/getDir.js
@@ -1,19 +1,18 @@
-function post(URL, PARAMS) {        
-    var temp = document.createElement("form");        
-    temp.action = URL;        
-    temp.method = "post";        
-    temp.style.display = "none";        
-    for (var x in PARAMS) {        
-        var opt = document.createElement("textarea");        
-        opt.name = x;        
-        opt.value = PARAMS[x];        
-        // alert(opt.name)        
-        temp.appendChild(opt);        
-    }        
-    document.body.appendChild(temp);        
-    temp.submit();        
-    return temp;        
-} 
-var url ='http://10.171.6.141:8080/CityObjectServer/terriajs/getFileList.php';
-var params = {projectName:'SPKNGEN'}
-alert(post(url,params))
+function getAllGISProjectMeta() {
+    $.ajax({
+        url: "http://10.171.6.157:8280/CityObjectServer/query/getUserGISProjects.php",
+        type: "POST",
+        data: {"owner": "asf@qq.com"},
+        dataType: "text",
+        success: function(data) {
+            var projects = JSON.parse(data);
+            // console.log(projects);
+            for (var i = projects.length - 1; i >= 0; i--) {
+                      var projectName = projects[i]['projectName'];
+                      var sideMenu = document.getElementById('projects');
+                      var inner = '<li><a href="#"><i class="fa fa-wrench fa-fw"></i>'+projectName+'<span class="fa arrow"></span></a></li>';
+                      sideMenu.innerHTML +=inner;
+                  }      
+        }
+    });
+}
