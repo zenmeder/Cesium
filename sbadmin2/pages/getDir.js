7c7b9b2fe3bfa68a8c35fea54e81a79852c87230
zenmeder <zenmeder@gmail.com>
2017-03-09 14:50:38 +0800

    default

--- sbadmin2/pages/getDir.js
+++ sbadmin2/pages/getDir.js
@@ -1,3 +1,17 @@
+function getProject(projectName, callback) {
+        $.ajax({
+        url: "http://10.171.6.157:8280/CityObjectServer/terriajs/getFileList.php",
+        type: "POST",
+        data: {projectName: projectName},
+        dataType: "json",
+        success: function (response) {
+            callback(response,projectName)
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
 function getAllGISProjectMeta() {
     $.ajax({
         url: "http://10.171.6.157:8280/CityObjectServer/query/getUserGISProjects.php",
@@ -6,12 +20,17 @@ function getAllGISProjectMeta() {
         dataType: "text",
         success: function(data) {
             var projects = JSON.parse(data);
-            // console.log(projects);
             for (var i = projects.length - 1; i >= 0; i--) {
-                      var projectName = projects[i]['projectName'];
-                      var sideMenu = document.getElementById('projects');
-                      var inner = '<li><a href="#"><i class="fa fa-wrench fa-fw"></i>'+projectName+'<span class="fa arrow"></span></a></li>';
-                      sideMenu.innerHTML +=inner;
+                      var projectName = projects[i]['projectName'];                      // to delete
+                      if(projectName == 'SPKNGEN' || projectName == 'JX4BKQ'|| projectName == 'LY76IF'){
+                            getProject(projectName, function(response,projectName){
+                                var inner = '<li><a href="#"><i class="fa fa-wrench fa-fw"></i>'+projectName+'<span class="fa arrow"></span></a></li>';
+                                var sideMenu = document.getElementById('projects');
+                                sideMenu.innerHTML +=inner;
+                                console.log(response);
+                                console.log(projectName);
+                            });
+                      }
                   }      
         }
     });
